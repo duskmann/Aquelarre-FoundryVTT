71f3af872c9870a75b832b1a935c630a39bd107d
Shyam <shyam.b.sridar@gmail.com>
2022-10-02 18:19:06 +0200

    Initial Upload

---
+++ module/helpers/config.mjs
@@ -0,0 +1,23 @@
+export const AQUELARRE = {};
+
+/**
+ * The set of Ability Scores used within the sytem.
+ * @type {Object}
+ */
+ AQUELARRE.abilities = {
+  "str": "AQUELARRE.AbilityStr",
+  "dex": "AQUELARRE.AbilityDex",
+  "con": "AQUELARRE.AbilityCon",
+  "int": "AQUELARRE.AbilityInt",
+  "wis": "AQUELARRE.AbilityWis",
+  "cha": "AQUELARRE.AbilityCha"
+};
+
+AQUELARRE.abilityAbbreviations = {
+  "str": "AQUELARRE.AbilityStrAbbr",
+  "dex": "AQUELARRE.AbilityDexAbbr",
+  "con": "AQUELARRE.AbilityConAbbr",
+  "int": "AQUELARRE.AbilityIntAbbr",
+  "wis": "AQUELARRE.AbilityWisAbbr",
+  "cha": "AQUELARRE.AbilityChaAbbr"
+};
